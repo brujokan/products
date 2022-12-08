465a777d788b84455a2a61852393518a03e39e8f
brujokan <luis.manzanaro@gmail.com>
2022-12-08 13:13:14 +0100

    Fixes

--- src/main/kotlin/com/soma/bookstore/products/infrastructure/messaging/producer/ProductProducerImpl.kt
+++ src/main/kotlin/com/soma/bookstore/products/infrastructure/messaging/producer/ProductProducerImpl.kt
@@ -5,6 +5,7 @@ import com.soma.bookstore.products.domain.mapper.Mapper
 import com.soma.bookstore.products.domain.messaging.ProductProducer
 import com.soma.bookstore.products.domain.model.Product
 import com.soma.bookstore.products.infrastructure.messaging.model.ProductMessage
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -18,10 +19,13 @@ class ProductProducerImpl(
     private val objectMapper: ObjectMapper
 ): ProductProducer {
 
+    private val logger = KotlinLogging.logger {  }
+
     override fun send(product: Product) {
         val message: String = objectMapper.writeValueAsString(
             messageMapper.map(product)
         )
         kafkaTemplate.send(topic, message)
+        logger.info { message }
     }
 }
